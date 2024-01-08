5041a1042af87df1b74053fe696913c90d7a0b5e
vabenil <victorabenilfernandez@gmail.com>
2024-01-08 02:48:20 +0200

    Update a couple of stuff

--- source/app.d
+++ source/app.d
@@ -3,33 +3,37 @@ import std.stdio;
 import bindbc.sdl;
 import bindbc.opengl;
 
+import inmath;
+
 import camera;
 import window;
 import sdl_ehandler;
+import world;
 
 import common.color;
 import common.types     : IVec3 = ivec3;
-
-import inmath;
-
-
 import voxel_grid.chunk;
 import voxel_grid.voxel;
 
 import voxel_renderer.gl_renderer;
 import voxel_renderer.gl_device;
 
-alias MChunk = VoxelChunk!(Voxel, 5);
+alias MChunk = VoxelChunk!(Voxel, 6);
 
 struct GState
 {
-    enum int WINDOW_WIDTH = 600;
-    enum int WINDOW_HEIGHT = 400;
+    /* enum int WINDOW_WIDTH = 600; */
+    /* enum int WINDOW_HEIGHT = 400; */
+    /* enum int WINDOW_WIDTH = 960; */
+    /* enum int WINDOW_HEIGHT = 540; */
+    enum int WINDOW_WIDTH = 1152;
+    enum int WINDOW_HEIGHT = 648;
 
     bool quit = false;
     Camera camera;
     Window win;
     VoxelRenderer!MChunk renderer;
+    World!MChunk world;
 }
 
 // TODO: perhaps pass this as an argument instead of global
@@ -99,25 +103,30 @@ void init_graphics()
     gstate.win = Window("Voxel engine", GState.WINDOW_WIDTH, GState.WINDOW_HEIGHT);
 
     SDL_SetRelativeMouseMode(true); // wrap this in Window
-    glClearColor(0, 0, 0, 1); // wrap this in vadgl
+    glClearColor(0.3, 0.3, 0.3, 1); // wrap this in vadgl
 
     GLDevice device = new GLDevice();
     device.device_init();
 
     gstate.renderer = new VoxelRenderer!MChunk(device);
 
-    MChunk chunk;
-    MChunk chunk2;
-    foreach (j; 0..MChunk.size) {
-        foreach (i; 0..MChunk.size) {
-            /* auto color = Color4b(cast(ubyte)((i+1) * 8), cast(ubyte)((j+1) * 8), 0); */
-            chunk[i, 0, j] = Voxel(Color4b.GREEN.to_hex);
-            chunk2[i, 0, j] = Voxel(Color4b.BLUE.to_hex);
-        }
+    /* MChunk chunk; */
+    /* MChunk chunk2; */
+    /* foreach (j; 0..MChunk.size) { */
+    /*     foreach (i; 0..MChunk.size) { */
+    /*         /1* auto color = Color4b(cast(ubyte)((i+1) * 8), cast(ubyte)((j+1) * 8), 0); *1/ */
+    /*         chunk[i, 0, j] = Voxel(Color4b.GREEN.to_hex); */
+    /*         chunk2[i, 0, j] = Voxel(Color4b.BLUE.to_hex); */
+    /*     } */
+    /* } */
+
+    /* gstate.renderer.commit_chunk(chunk, IVec3(0, 0, 0)); */
+    /* gstate.renderer.commit_chunk(chunk2, IVec3(1, 0, 0)); */
+    foreach (pos, chunk; gstate.world) {
+        /* writeln("chunk_pos - ", pos); */
+        gstate.renderer.commit_chunk(chunk, pos);
     }
 
-    gstate.renderer.commit_chunk(chunk, IVec3(0, 0, 0));
-    gstate.renderer.commit_chunk(chunk2, IVec3(1, 0, 0));
     gstate.renderer.send_to_device();
 
     writeln("face count - ", gstate.renderer.mesh_buffer.face_meshes.length);
@@ -135,43 +144,35 @@ void main_loop()
         event_handler.handle_sdl_events(&gstate);
 
         gl_clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT).trust;
-        /* writeln(camera.pitch, ", ", camera.yaw); */
-        /* writeln(camera.pos); */
 
         /* camera.set_direction(); */
         gstate.camera.look_at();
 
         gstate.renderer.get_device().set_mpv_matrix(gstate.camera.mpv(), true);
-        gstate.renderer.render_chunk(IVec3(0, 0, 0));
-        gstate.renderer.render_chunk(IVec3(1, 0, 0));
+
+        foreach (pos, chunk; gstate.world) {
+            gstate.renderer.render_chunk(pos);
+        }
+        /* gstate.renderer.render_chunk(IVec3(1, 0, 0)); */
         /* renderer.get_device().render(); */
         gstate.win.swap_buffer();
     }
 }
-
-/* void create_class_diagram() */
-/* { */
-/*     import duml_class_gen; */
-/*     import voxel_grid.voxel_grid_array  : VoxelGridArray; */
-
-/*     alias Grid = VoxelGridArray!Chunk; */
-
-/*     static immutable string res = */
-/*         create_uml_diagram!(Grid); */
-
-/*     File diagram_f = File("diagram.dot", "w"); */
-/*     diagram_f.write(res); */
-/* } */
-
 void main()
 {
-    vec3 pos = vec3(15.0f, 4.0f, 0.0f);
+    vec3 pos = vec3(15.0f, 8.0f, 0.0f);
     vec3 target = vec3(1.0f * MChunk.size, 0.0f, 0.0f); // look at second chunk
 
     // set camera
     gstate.camera = Camera(pos, 60.0f, gstate.WINDOW_WIDTH, gstate.WINDOW_HEIGHT);
     gstate.camera.set_direction((target - gstate.camera.pos).normalized);
     gstate.camera.look_at(); // calculate view matrix
+
+    /* gstate.world.load_from_vox_file("./assets/SmallBuilding01.vox"); */
+    /* gstate.world.load_from_vox_file("./assets/11_SKELLINGTON_CHAMPION.vox"); */
+    /* gstate.world.load_from_vox_file("./assets/realistic_terrain.vox"); */
+    gstate.world.load_from_vox_file("./assets/Plane04.vox");
+    /* gstate.world.load_from_vox_file("./assets/11.vox"); */
 
     writeln(gstate.camera.pitch, ", ", gstate.camera.yaw);
     writefln("[%(%(%s, %),\n%)]", gstate.camera.view.matrix);
