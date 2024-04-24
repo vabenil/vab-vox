f160b067f23eb75dc7d57beb0bc4d49d1f8f255d
Victor Fernandez <victorabenilfernandez@gmail.com>
2024-04-24 17:24:17 +0300

    Update interface

--- voxel_renderer/source/voxel_renderer/renderer.d
+++ voxel_renderer/source/voxel_renderer/renderer.d
@@ -4,12 +4,14 @@ import voxel_renderer.device;
 import voxel_grid;
 import voxel_grid.bitchunk;
 
+import common   : ivec3, vec3;
+
 static immutable size_t chunk_M = 5;
 private alias BitChunk = VoxelBitChunk!(BitVoxel, chunk_M);
 
 interface VoxelRenderer(ChunkT)
 {
-    alias VoxelT = GridType.VoxelType;
+    alias VoxelT = ChunkT.VoxelType;
 
     VoxelDevice get_device();
     // TODO: pass camera
@@ -20,10 +22,13 @@ interface VoxelRenderer(ChunkT)
 
     void commit_chunk(ref const(ChunkT) chunk, ivec3 chunk_pos);
 
-    void commit_voxel(VoxelT voxel, ivec3 pos);
+    /* void commit_voxel(VoxelT voxel, ivec3 pos); */
 
-    // Chunk should already be in device by this point
-    void render_chunk(ivec3 chunk_pos);
+    void queue_chunk(ivec3 chunk_pos, ref ChunkT chunk);
 
-    void flush();
+    void send_to_device();
+
+    void render();
+
+    /* void flush(); */
 }
